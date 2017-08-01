f54450023bc0a1ab68e046438f22cf97ef2c13a8
wangyuankan <wangyuankan@100msh.com>
2017-08-01 16:54:54 +0800

    illuminate/pagination分页引入

--- application/controllers/Partner.php
+++ application/controllers/Partner.php
@@ -5,7 +5,16 @@ class PartnerController extends \Yaf\Controller_Abstract {
 
     public function indexAction() {
         $cate_id = $this->getRequest()->getQuery("cate_id", "1");
-        $list = DB::table('t_partner')->where("cate_id",$cate_id)->select('partner_id','partner_name','add_time')->get();
+        //$list = DB::table('t_partner')->where("cate_id",$cate_id)->select('partner_id','partner_name','add_time')->limit(10)->get();
+        $page = $this->getRequest()->getQuery("page", "1");
+        // 查询语句构造器进行分页
+        $list = DB::table('t_partner')->paginate(10,['*'],'page',$page);
+
+        // Eloquent 模型进行分页
+//        $list = Partner::with('category')->paginate(10,['*'],'page',$page);
+//        foreach ($list as $l) {
+//            var_dump($l->partner_name);
+//        }die;
         var_dump($list);die;
     }
 
